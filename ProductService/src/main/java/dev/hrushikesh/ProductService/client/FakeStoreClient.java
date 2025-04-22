631733a4c284cfee13fc8668f1067d3210470037
SpiderMan <hrushikesh.tk@gmail.com>
2025-04-22 22:29:25 +0530

    Added POST, PUT and DELETE mappings

--- ProductService/src/main/java/dev/hrushikesh/ProductService/client/FakeStoreClient.java
+++ ProductService/src/main/java/dev/hrushikesh/ProductService/client/FakeStoreClient.java
@@ -2,7 +2,12 @@ package dev.hrushikesh.ProductService.client;
 
 import dev.hrushikesh.ProductService.dto.FakeStoreProductDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpMethod;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpMessageConverterExtractor;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 @Component
@@ -21,6 +26,41 @@ public class FakeStoreClient {
         String getProduct = "https://fakestoreapi.com/products/" + productId;
         FakeStoreProductDTO response = restTemplate.getForObject(getProduct, FakeStoreProductDTO.class);
         return response;
+    }
+
+    public FakeStoreProductDTO createProduct(FakeStoreProductDTO input) {
+        String createProduct = "https://fakestoreapi.com/products";
+        FakeStoreProductDTO response = restTemplate.postForObject(createProduct, input, FakeStoreProductDTO.class);
+        return response;
+    }
+
+    public FakeStoreProductDTO replaceProduct(int productId, FakeStoreProductDTO input) {
+        String replaceProduct = "https://fakestoreapi.com/products/" + productId;
+        FakeStoreProductDTO response = requestForObject(replaceProduct, HttpMethod.PUT, input, FakeStoreProductDTO.class);
+        return response;
+    }
+
+    public Boolean deleteProduct(int productId) {
+        String deleteProduct = "https://fakestoreapi.com/products/" + productId;
+        try {
+            requestForObject(deleteProduct, HttpMethod.DELETE, null, FakeStoreProductDTO.class);
+            return true;
+        } catch (Exception exception) {
+            System.out.println(exception);
+            return false;
+        }
+    }
+
+//    public <T> T postForObject(String url, @Nullable Object request, Class<T> responseType, Object... uriVariables) throws RestClientException {
+//        RequestCallback requestCallback = this.httpEntityCallback(request, responseType);
+//        HttpMessageConverterExtractor<T> responseExtractor = new HttpMessageConverterExtractor(responseType, this.getMessageConverters(), this.logger);
+//        return (T)this.execute(url, HttpMethod.POST, requestCallback, responseExtractor, uriVariables);
+//    }
+
+    private <T> T requestForObject(String url, HttpMethod httpMethod, @Nullable Object request, Class<T> responseType, Object... uriVariables) throws RestClientException {
+        RequestCallback requestCallback = restTemplate.httpEntityCallback(request, responseType);
+        HttpMessageConverterExtractor<T> responseExtractor = new HttpMessageConverterExtractor(responseType, restTemplate.getMessageConverters());
+        return restTemplate.execute(url, httpMethod, requestCallback, responseExtractor, (Object[])uriVariables);
     }
 
 }
--- ProductService/src/main/java/dev/hrushikesh/ProductService/controller/ProductController.java
+++ ProductService/src/main/java/dev/hrushikesh/ProductService/controller/ProductController.java
@@ -3,9 +3,7 @@ package dev.hrushikesh.ProductService.controller;
 import dev.hrushikesh.ProductService.dto.FakeStoreProductDTO;
 import dev.hrushikesh.ProductService.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class ProductController {
@@ -21,5 +19,20 @@ public class ProductController {
     @GetMapping("/product/{id}")
     public FakeStoreProductDTO getProduct(@PathVariable("id") int id) {
         return productService.getProductById(id);
+    }
+
+    @PostMapping("/product")
+    public FakeStoreProductDTO createProduct(@RequestBody FakeStoreProductDTO fakeStoreProductDTO) {
+        return productService.createProduct(fakeStoreProductDTO);
+    }
+
+    @PutMapping("/product/{id}")
+    public FakeStoreProductDTO replaceProduct(@PathVariable("id") int id, @RequestBody FakeStoreProductDTO fakeStoreProductDTO) {
+        return productService.replaceProduct(id, fakeStoreProductDTO);
+    }
+
+    @DeleteMapping("/product/{id}")
+    public Boolean deleteProduct(@PathVariable("id") int id) {
+        return productService.deleteProduct(id);
     }
 }
